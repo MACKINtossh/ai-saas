0d2b5a81a9d4f4fee14f22b9612458eb283d624d
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-07-30 08:00:42 -0400

    implemented api limit functionality

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -2,6 +2,8 @@ import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { NextResponse } from "next/server";
 import { Configuration, OpenAIApi } from "openai";
+import { incrementApiLimit, checkApiLimit } from "@/lib/api-limit";
+
 
 const configuration = new Configuration({
   apiKey: process.env.OPEN_API_KEY,
@@ -33,11 +35,19 @@ export async function POST(req: Request) {
       return new NextResponse("Resolution is required", { status: 400 });
     }
 
+    const freeTrial = await checkApiLimit();
+
+    if (!freeTrial) {
+      return new NextResponse("Free trial has expired.", { status: 403 });
+    }
+
     const response = await openai.createImage({
       prompt: prompt,
       n: parseInt(amount, 10),
       size: resolution,
     });
+
+    await incrementApiLimit();
 
     return NextResponse.json(response.data.data);
   } catch (error) {
