eef152db02d6a1660803eb4df473de162265388e
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-07-31 06:03:27 -0400

    removed z-index from sidebar component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -11,7 +11,7 @@ const Dashboardlayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full relative">
       {/* SIDEBAR */}
-      <div className="hidden h-full md:w-72 md:flex md:flex-col md:fixed  md:inset-y-0 z-[80] bg-gray-900">
+      <div className="hidden h-full md:w-72 md:flex md:flex-col md:fixed  md:inset-y-0 bg-gray-900">
         <Sidebar apiLimitCount={apiLimitCount} />
       </div>
       <main className="md:pl-72 ">
