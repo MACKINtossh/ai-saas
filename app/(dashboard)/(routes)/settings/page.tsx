9ecd11323a93989163c249ec11ff30b3a8a21a3a
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-08-01 10:26:46 -0400

    created the initial settings page

---
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -0,0 +1,31 @@
+import Heading from "@/components/Heading";
+import { checkSubscription } from "@/lib/subscription";
+import { Settings } from "lucide-react";
+import React from "react";
+
+type Props = {};
+
+const SettingsPage = async (props: Props) => {
+  const isPro = await checkSubscription();
+
+  return (
+    <div>
+      <Heading
+        title="Settings"
+        description="Manage Account Settings"
+        icon={Settings}
+        iconColor="text-gray-700"
+        bgColor="bg-gray-700/10"
+      />
+      <div className="px-4 lg:px-8 space-y-4">
+        <div className="text-muted-foreground text-sm">
+          {isPro
+            ? "You are currently on the Pro plan"
+            : "You are currently on the free plan"}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SettingsPage;
